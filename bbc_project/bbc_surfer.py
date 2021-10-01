d77578a104b6be581aa69b96e4d79a499507fcbb
Osama <oabu@spartaglobal.com>
2021-10-01 19:22:33 +0100

    last update

--- bbc_project/bbc_surfer.py
+++ bbc_project/bbc_surfer.py
@@ -1,10 +1,12 @@
 import requests
 
-request_bbc = requests.get("https://www.bbc.co.uk")
+request_bbc = requests.get("https://www.bbc.co.uk/")
 
 print("----------- The status_code -----------")
 print(request_bbc.status_code)
+print(request_bbc.headers)
+#print(request_bbc.json())
 input()
 
 print("----------- The content ---------------")
-print(request_bbc.content)
+#print(request_bbc.content)
